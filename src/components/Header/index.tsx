26738602e1d0c61ea1816d8f2c93fabd06ec7278
Tarcizio Barbosa - Dislog <tarcizio.barbosa@outlook.com>
2021-10-26 08:58:25 -0300

    change title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <div className="mt-10 flex justify-between">
       <h1 className="font-opens text-4xl font-bold text-green-700">
-        Eleições CIPA - 2021
+        Eleições 2021 Para CIPA 2022
       </h1>
       {/* <button className='w-36 h-12 bg-gray-100 font-opens font-bold text-sm text-gray-500 border-2 hover:border-gray-500'>ENTRAR</button> */}
     </div>
